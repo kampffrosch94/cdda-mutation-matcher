23ca94b290e7fa969c0814a6b566204915d0124a
kampffrosch <johanneskar.dev@gmail.com>
2020-07-02 19:42:23 +0200

    more lists and treshold stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::BufReader;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -11,6 +11,8 @@ struct Mutation {
     cancels: Vec<String>,
     #[serde(default)]
     category: Vec<String>,
+    #[serde(default)]
+    threshreq: Vec<String>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -18,20 +20,62 @@ struct Name {
     str: String,
 }
 
-fn main() -> anyhow::Result<()>{
+fn main() -> anyhow::Result<()> {
     let s: String = std::fs::read_to_string("mutations.json")?;
     let mut data: Vec<Mutation> = serde_json::from_str(&s)?;
     data.sort_by_key(|it| it.name.str.clone());
+
+    println!("Mutation cancels:");
     for d in data.iter() {
-        let mut cancellers = vec![];
+        let mut cancellers = Vec::new();
         for x in data.iter().filter(|it| !it.category.is_empty()) {
             if x.cancels.contains(&d.id) {
-                cancellers.push(x.name.str.clone())
+                if !x.threshreq.is_empty() {
+                    cancellers.push(format!("{} ({})", x.name.str.clone(), x.threshreq[0]));
+                } else {
+                    cancellers.push(x.name.str.clone())
+                }
             }
         }
         if !cancellers.is_empty() {
             println!("{} cancelled by {:?}", d.name.str, cancellers);
         }
+    }
+    println!("\n\n\nMutation categories");
+    let mut categories = HashSet::new();
+    for d in data.iter().filter(|it| !it.category.is_empty()) {
+        if d.threshreq.is_empty() {
+            println!("{} in {:?}", d.name.str, d.category);
+        } else {
+            println!("{} in {:?} (threshold required)", d.name.str, d.category);
+        }
+        for c in d.category.iter() {
+            categories.insert(c.clone());
+        }
+    }
+
+    println!("\n\n\nMutation by category");
+    let mut categories: Vec<_> = categories.iter().collect();
+    categories.sort();
+
+    for c in categories {
+        let mut mutations = Vec::new();
+        let mut threshold_mutations = Vec::new();
+        for d in data.iter() {
+            if d.category.contains(c) {
+                if !d.threshreq.is_empty() {
+                    threshold_mutations.push(d.name.str.clone());
+                } else {
+                    mutations.push(d.name.str.clone());
+                }
+            }
+        }
+        println!("\n{}", c);
+        mutations.sort();
+        mutations.iter().for_each(|it| println!("{}", it));
+        println!("\n{} post threshold", c);
+        threshold_mutations.sort();
+        threshold_mutations.iter().for_each(|it| println!("{}", it));
     }
     Ok(())
 }
